42187f26465c3ff71ee06f8d5feb3f856e5985a5
Cerek Hillen <cerekh@gmail.com>
2023-07-23 01:46:04 -0700

    maybe move things over successfully?

--- main.go
+++ main.go
@@ -5,12 +5,9 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/crockeo/schoner/pkg/graph"
-	"github.com/crockeo/schoner/pkg/phases"
-	"github.com/crockeo/schoner/pkg/phases/declarations"
+	"github.com/crockeo/schoner/pkg/phases/fileinfo"
 	"github.com/crockeo/schoner/pkg/phases/references"
 	"github.com/crockeo/schoner/pkg/walk"
-	"github.com/crockeo/schoner/pkg/visualize"
 )
 
 func main() {
@@ -21,68 +18,25 @@ func main() {
 }
 
 func mainImpl() error {
+	walkOptions := walk.WithIgnoreDirs(".git")
 	for _, root := range os.Args[1:] {
 		root, err := filepath.Abs(root)
 		if err != nil {
 			return err
 		}
 
-		decls, err := phases.FindDeclarations(root, walk.WithIgnoreDirs(".git"))
+		fileInfos, err := fileinfo.FindFileInfos(root, walkOptions)
 		if err != nil {
 			return err
 		}
 
-		refs, err := phases.FindReferences(root, decls, walk.WithIgnoreDirs(".git"))
+		referenceGraph, err := references.BuildReferenceGraph(root, fileInfos, walkOptions)
 		if err != nil {
 			return err
 		}
 
-		// renderDecls(decls)
-		// renderRefs(refs)
-
-		referenceGraph := graph.NewGraph[declarations.Declaration]()
-		for _, fileRefs := range refs {
-			for decl, targets := range fileRefs.References {
-				for target := range targets {
-					referenceGraph.AddEdge(decl, target)
-				}
-			}
-		}
-
-		if err := visualize.Visualize(
-			fmt.Sprintf("%s.svg", filepath.Base(root)),
-			referenceGraph,
-		); err != nil {
-			return err
-		}
+		_ = referenceGraph
 	}
 
 	return nil
-}
-
-func renderDecls(decls map[string]*declarations.Declarations) {
-	for filename, fileDecls := range decls {
-		fmt.Println(filename)
-		for decl := range fileDecls.Declarations {
-			fmt.Println("  ", decl)
-		}
-		fmt.Println("   ---")
-		for importDecl := range fileDecls.Imports {
-			fmt.Println("  ", fmt.Sprintf("`%s`", importDecl.Name), importDecl.Path)
-		}
-		fmt.Println()
-		fmt.Println()
-	}
-}
-
-func renderRefs(refs map[string]*references.References) {
-	for filename, fileReferences := range refs {
-		fmt.Println(filename)
-		for from, tos := range fileReferences.References {
-			fmt.Println("  ", from, "->")
-			for to := range tos {
-				fmt.Println("    ", to)
-			}
-		}
-	}
 }
--- pkg/phases/references/references.go
+++ pkg/phases/references/references.go
@@ -5,77 +5,97 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/crockeo/schoner/pkg/astutil"
-	"github.com/crockeo/schoner/pkg/phases/declarations"
-	"github.com/crockeo/schoner/pkg/set"
+	"github.com/crockeo/schoner/pkg/graph"
+	"github.com/crockeo/schoner/pkg/phases/fileinfo"
+	"github.com/crockeo/schoner/pkg/walk"
+	"golang.org/x/mod/modfile"
 )
 
-// find all references in a file
+// TODO: find all references in a file
 //
 // 3 kinds of references:
 //
-// - static local references, where an ast.Ident references a delcaration in the current file
+// - [x] static local references, where an ast.Ident references a delcaration in the current file
 //
-// - static imorted references, where an ast.SelectorExpr references a declaration in an imported file
+// - [x] static imported references, where an ast.SelectorExpr references a declaration in an imported file
 //
-// - dynamic references, where an ast.SelectorExpr references a field or method on another
+// - [ ] dynamic references, where an ast.SelectorExpr references a field or method on another
 
-type ReferencesContext struct {
-	ProjectRoot       string
-	GoModRoot         string
-	Declarations      map[string]*declarations.Declarations
-	DeclarationLookup map[string]map[string]declarations.Declaration
+type Declaration struct {
+	Parent *fileinfo.FileInfo
+	Name   string
 }
 
-type References struct {
-	References map[declarations.Declaration]set.Set[declarations.Declaration]
-}
-
-func (r *References) Add(from declarations.Declaration, to declarations.Declaration) {
-	if from == to {
-		return
-	}
-
-	if _, ok := r.References[from]; !ok {
-		r.References[from] = set.NewSet[declarations.Declaration]()
-	}
-	r.References[from].Add(to)
-}
-
-func FindReferences(
-	ctx *ReferencesContext,
-	fileset *token.FileSet,
-	filename string,
-	contents []byte,
-) (*References, error) {
-	ourDeclarations, ok := ctx.Declarations[filename]
-	if !ok {
-		return nil, fmt.Errorf("no declarations for file %s", filename)
-	}
-	ourPackage, err := findPackagePath(ctx.ProjectRoot, ctx.GoModRoot, filename)
+func BuildReferenceGraph(
+	root string,
+	fileInfos map[string]*fileinfo.FileInfo,
+	option walk.Option,
+) (graph.Graph[Declaration], error) {
+	builder, err := newReferenceGraphBuilder(root, fileInfos)
 	if err != nil {
 		return nil, err
 	}
-	ourDeclarationsByName := ctx.DeclarationLookup[ourPackage]
-
-	fileAst, err := parser.ParseFile(fileset, filename, contents, 0)
-	if err != nil {
-		return nil, err
+	fileset := token.NewFileSet()
+	for path, fileInfo := range fileInfos {
+		contents, err := os.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+		fileAst, err := parser.ParseFile(fileset, path, contents, 0)
+		if err != nil {
+			return nil, err
+		}
+		if err := builder.Visit(path, fileInfo, fileAst); err != nil {
+			return nil, err
+		}
 	}
+	return builder.ReferenceGraph, nil
+}
 
-	references := &References{References: map[declarations.Declaration]set.Set[declarations.Declaration]{}}
+type referenceGraphBuilder struct {
+	ReferenceGraph    graph.Graph[Declaration]
+	RootModule        string
+	DeclarationLookup map[string]map[string]Declaration
+}
+
+func newReferenceGraphBuilder(root string, fileInfos map[string]*fileinfo.FileInfo) (*referenceGraphBuilder, error) {
+	goModContents, err := os.ReadFile(filepath.Join(root, "go.mod"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to find go module root: %w", err)
+	}
+	modulePath := modfile.ModulePath(goModContents)
+	if modulePath == "" {
+		return nil, fmt.Errorf("failed to parse module path from go module: %w", err)
+	}
+	declarationLookup, err := makeDeclarationLookup(root, modulePath, fileInfos)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create declaration lookup: %w", err)
+	}
+	return &referenceGraphBuilder{
+		ReferenceGraph:    graph.NewGraph[Declaration](),
+		RootModule:        modulePath,
+		DeclarationLookup: declarationLookup,
+	}, nil
+}
+
+func (rgb *referenceGraphBuilder) Visit(filename string, fileInfo *fileinfo.FileInfo, fileAst *ast.File) error {
 	path := []ast.Node{}
-	err = astutil.Walk(fileAst, func(node ast.Node) error {
+	err := astutil.Walk(fileAst, func(node ast.Node) error {
 		if node == nil {
 			path = path[:len(path)-1]
 			return nil
 		}
 		defer func() { path = append(path, node) }()
 
-		from := declarations.Declaration{Filename: filename}
+		from := Declaration{
+			Parent: fileInfo,
+			Name:   "root",
+		}
 		container, err := astutil.OuterDeclName(path)
 		if err == nil {
 			from.Name = container
@@ -83,77 +103,90 @@ func FindReferences(
 
 		switch node := node.(type) {
 		case *ast.Ident:
-			reference, ok := findIdentReference(ourDeclarationsByName, node)
+			target, ok := rgb.identReference(fileInfo, node)
 			if ok {
-				references.Add(from, reference)
+				rgb.ReferenceGraph.AddEdge(from, target)
 			}
 		case *ast.SelectorExpr:
-			reference, ok := findSelectorReference(ctx, ourDeclarations, node)
+			target, ok := rgb.selectorReference(fileInfo, node)
 			if ok {
-				references.Add(from, reference)
+				rgb.ReferenceGraph.AddEdge(from, target)
 			}
 		}
 		return nil
 	})
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	return references, nil
+	return nil
 }
 
-func findIdentReference(ourDeclarationsByName map[string]declarations.Declaration, ident *ast.Ident) (declarations.Declaration, bool) {
-	decl, ok := ourDeclarationsByName[ident.Name]
-	return decl, ok
+func (rgb *referenceGraphBuilder) identReference(currentFileInfo *fileinfo.FileInfo, ident *ast.Ident) (Declaration, bool) {
+	if !currentFileInfo.Declarations.Contains(ident.Name) {
+		return Declaration{}, false
+	}
+	return Declaration{
+		Parent: currentFileInfo,
+		Name: ident.Name,
+	}, true
 }
 
-func findSelectorReference(
-	ctx *ReferencesContext,
-	ourDeclarations *declarations.Declarations,
-	selector *ast.SelectorExpr,
-) (declarations.Declaration, bool) {
+func (rgb *referenceGraphBuilder) selectorReference(currentFileInfo *fileinfo.FileInfo, selector *ast.SelectorExpr) (Declaration, bool) {
 	ident, ok := selector.X.(*ast.Ident)
 	if !ok {
-		return declarations.Declaration{}, false
+		return Declaration{}, false
 	}
 	selectorName := ident.Name
-	importDecl, ok := findImportDecl(ourDeclarations.Imports, selectorName)
-	if !ok {
-		return declarations.Declaration{}, false
+
+	foundImport := false
+	var importDecl fileinfo.Import
+	for importDecl = range currentFileInfo.Imports {
+		if selectorName == importDecl.Name {
+			foundImport = true
+			break
+		}
+	}
+	if !foundImport {
+		return Declaration{}, false
 	}
 
-	moduleDecls, ok := ctx.DeclarationLookup[importDecl.Path]
+	moduleDecls, ok := rgb.DeclarationLookup[importDecl.Path]
 	if !ok {
-		return declarations.Declaration{}, false
+		return Declaration{}, false
 	}
 	decl, ok := moduleDecls[selector.Sel.Name]
 	return decl, ok
 }
 
-func findImportDecl(imports set.Set[declarations.Import], selectorName string) (*declarations.Import, bool) {
-	for importDecl := range imports {
-		if selectorName == importDecl.Name {
-			return &importDecl, true
+func makeDeclarationLookup(
+	root string,
+	rootModule string,
+	fileInfos map[string]*fileinfo.FileInfo,
+) (map[string]map[string]Declaration, error) {
+	// module -> symbol -> declaration
+	declarationLookup := map[string]map[string]Declaration{}
+	for filename, fileInfo := range fileInfos {
+		if !strings.HasPrefix(filename, root) {
+			return nil, fmt.Errorf("file %s does not start with root %s", filename, root)
+		}
+		suffix := filename[len(root):]
+		suffix = strings.TrimPrefix(suffix, "/")
+
+		module := rootModule
+		if suffix != "" {
+			module = filepath.Join(module, filepath.Dir(suffix))
 		}
 
-		lastPart := filepath.Base(importDecl.Path)
-		if selectorName == lastPart {
-			return &importDecl, true
+		if _, ok := declarationLookup[module]; !ok {
+			declarationLookup[module] = map[string]Declaration{}
+		}
+
+		for decl := range fileInfo.Declarations {
+			declarationLookup[module][decl] = Declaration{
+				Parent: fileInfo,
+				Name:   decl,
+			}
 		}
 	}
-	return nil, false
-}
-
-func findPackagePath(projectRoot string, goModRoot string, filename string) (string, error) {
-	if !strings.HasPrefix(filename, projectRoot) {
-		return "", fmt.Errorf("filename %s is not in root %s", filename, projectRoot)
-	}
-	filename = filename[len(projectRoot):]
-	filename = strings.TrimSuffix(filename, "/")
-
-	if filename == "" {
-		return goModRoot, nil
-	}
-	filename = filepath.Dir(filename)
-	return filepath.Join(goModRoot, filename), nil
+	return declarationLookup, nil
 }
